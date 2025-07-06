fd80a0171935d3f5daaacaf811a11c51336ca3e3
ashif <hasanat153@gmail.com>
2025-07-06 15:21:20 +0600

    fgd

---
+++ neApi/neApi/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using neApi.model;
+using neApi.repo;
+
+namespace neApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountRepository accountRepository;
+
+        public AccountController(IAccountRepository accountRepository)
+        {
+            this.accountRepository = accountRepository;
+        }
+
+        [HttpPost("SignUp")]
+        public async Task<IActionResult> SignUp([FromBody] SignUp signUp)
+        {
+          var result=  await accountRepository.SignUpAsync(signUp);
+            if (result.Succeeded)
+            {
+                 return Ok("Success");
+            }
+            return Unauthorized();
+        } 
+        [HttpPost("SignIn")]
+        public async Task<IActionResult> SignIn([FromBody] SignIn signIn)
+        {
+          var result=  await accountRepository.SignInAsync(signIn);
+            if (string.IsNullOrEmpty(result))
+            {
+                 return Unauthorized();
+            }
+            return Ok(result);
+        }
+
+
+
+
+    }
+}
---
+++ neApi/neApi/repo/AccountRepository.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using neApi.model;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace neApi.repo
+{
+    public class AccountRepository:IAccountRepository
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManger;
+        private readonly IConfiguration configuration;
+
+        public AccountRepository(UserManager<ApplicationUser> userManager,RoleManager<IdentityRole> roleManager,SignInManager<ApplicationUser> signInManger,IConfiguration configuration)
+        {
+            this.userManager = userManager;
+            this.signInManger = signInManger;
+            this.configuration = configuration;
+        }
+
+
+        public async Task<IdentityResult>  SignUpAsync(SignUp signUp)
+        {
+            var user=new ApplicationUser() { FirstName=signUp.FirstName,
+                LastName=signUp.LastName,
+                Email=signUp.Email,
+                UserName=signUp.Email };
+
+
+           return await userManager.CreateAsync(user,signUp.Password);
+
+        }
+
+        public async Task<string> SignInAsync(SignIn signIn)
+        {
+            var result = await signInManger.PasswordSignInAsync(signIn.Email, signIn.Password, false, false);
+            if (!result.Succeeded)
+            {
+                return null;
+                
+            }
+            var authClaim = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name,signIn.Email), 
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()), 
+
+            };
+            var authSigninkey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(configuration["JWT:Secret"]));
+
+            var token = new JwtSecurityToken(
+                issuer: configuration["JWT:ValidIssuer"],
+                audience: configuration["JWT:ValidAudience"],
+                expires:DateTime.Now.AddDays(1),
+                claims: authClaim,
+                signingCredentials:new SigningCredentials(authSigninkey,SecurityAlgorithms.HmacSha256Signature)
+                );
+
+        return    new JwtSecurityTokenHandler().WriteToken(token);
+
+
+        }
+
+
+
+    }
+}
